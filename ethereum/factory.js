c032ee489254745f7ef98e3eec05aa96bef198fe
staticGuru <79417096+staticGuru@users.noreply.github.com>
2022-04-14 14:51:11 +0530

    create the campaign factory instace creation

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from "web3";
+import compiledFactory from "./build/CampaignFactory.json";
+
+const instance = new web3.eth.Contract(
+  JSON.parse(compiledFactory.interface),
+  '0xc53087338d59964e152e071f5d5b45622f370abb'
+);
+
+export default instance;
